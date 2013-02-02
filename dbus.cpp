34606a74285756c7ded84ae75e6ef5aa3f31307b
Majo <marian.varaga@gmail.com>
2013-02-02 21:48:17 +0000

    add basic connection to dbus

---
+++ dbus.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <QtCore/QCoreApplication>
+#include <QtDBus/QtDBus>
+#include "dbus.h"
+
+
+
+int Dbus::execute()
+{
+    qDBusRegisterMetaType<QMap<QString,QString> >();
+
+  
+
+    QString service("org.freedesktop.problems");
+
+    QDBusConnection bus = QDBusConnection::systemBus();
+    QDBusInterface *interface = new QDBusInterface(service,
+            "/org/freedesktop/problems",
+            "org.freedesktop.problems",
+            bus);
+
+    QDBusReply<QStringList> reply = interface->call("GetProblems");
+    QStringList stats("executable");
+
+    if (reply.isValid())
+    {
+        QStringList list = reply.value();
+
+
+        for (int i = 0; i < list.size(); ++i)
+        {
+            printf("%s\n", qPrintable(list.at(i)));
+
+            QDBusReply<QMap<QString,QString> > replyInfo = interface->call("GetInfo", list.at(i),stats);
+
+            if(replyInfo.isValid()) {
+                printf("\t%s\n", qPrintable(replyInfo.value().value("executable")));
+            } else
+            {
+                fprintf(stderr, "replyInfo failed: %s\n", qPrintable(replyInfo.error().message()));
+            }
+        }
+
+    }
+    else {
+        fprintf(stderr, "Call failed: %s\n", qPrintable(reply.error().message()));
+        return 1;
+    }
+    
+    return 0;
+}
+
---
+++ dbus.h
@@ -0,0 +1,23 @@
+#ifndef DBUS_H
+#define DBUS_H
+
+#include <stdio.h>
+#include <QtCore/QCoreApplication>
+#include <QtDBus/QtDBus>
+
+typedef QMap <QString, QString> MyMap;
+Q_DECLARE_METATYPE(MyMap)
+
+class Dbus
+{
+  public:
+    int execute();
+
+  private:
+
+
+};
+
+
+#endif
+    
